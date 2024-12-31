e7731c6004863bb488af1adc89479847b31318bc
aymen <khazriaymen93@gmail.com>
2024-12-31 11:38:44 +0100

    get data from the first activity and set it to the text view in the second activity

--- app/src/main/java/com/example/intentexplicit/MainActivity2.java
+++ app/src/main/java/com/example/intentexplicit/MainActivity2.java
@@ -1,6 +1,8 @@
 package com.example.intentexplicit;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +17,11 @@ public class MainActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main2);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+
+        Intent intent = getIntent(); //TODO remark where the intent is created now
+        String message  = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView  = findViewById(R.id.text_message);
+        textView.setText(message);
     }
 }
