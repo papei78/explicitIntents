64ea69dd2ea324437c645fde693b493b0ae66bcb
aymen <khazriaymen93@gmail.com>
2024-12-31 11:26:07 +0100

    add the extra and bring the editText

--- app/src/main/java/com/example/intentexplicit/MainActivity.java
+++ app/src/main/java/com/example/intentexplicit/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,7 +14,8 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    public static final String EXTRA_MESSAGE = "key.for.extra.MESSAGE";
+    private EditText mMessageEditText;
     private static final String LOG_TAG =
         MainActivity.class.getSimpleName();
 
@@ -23,16 +25,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        mMessageEditText = findViewById(R.id.editText_main);
+
+
+
+
     }
 
     public void launchSecondActivity(View view) {
         Log.d(LOG_TAG, "Button clicked hhhhh!");
         Intent intent = new Intent(this, MainActivity2.class);
         startActivity(intent);
+        String message = mMessageEditText.getText().toString();
     }
 }
