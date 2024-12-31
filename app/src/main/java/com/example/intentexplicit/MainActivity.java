87ba9df99f8121e87f162461a975b381cb743811
aymen <khazriaymen93@gmail.com>
2024-12-31 14:36:51 +0100

    prepare the textViews containers

--- app/src/main/java/com/example/intentexplicit/MainActivity.java
+++ app/src/main/java/com/example/intentexplicit/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +15,10 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private TextView mReplyHeadTextView;
+    private TextView mReplyTextView;
+    public static final int TEXT_REQUEST = 1;
     public static final String EXTRA_MESSAGE = "key.for.extra.MESSAGE";
     private EditText mMessageEditText;
     private static final String LOG_TAG =
@@ -29,6 +34,8 @@ public class MainActivity extends AppCompatActivity {
         mMessageEditText = findViewById(R.id.editText_main);
 
 
+        mReplyHeadTextView = findViewById(R.id.text_header_reply);
+        mReplyTextView = findViewById(R.id.text_message_reply);
 
 
     }
